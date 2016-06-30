9c8b31f4d3095f93c8a72d823e296327ec59da8b
Naszi <naszi75@gmail.com>
2016-06-30 12:03:53 +0200

    Version 1.1

--- src/hu/naszi/example/basicex/BasicEx.java
+++ src/hu/naszi/example/basicex/BasicEx.java
@@ -1,8 +1,10 @@
 package hu.naszi.example.basicex;
 
+import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class Surface extends JPanel {
@@ -19,6 +21,28 @@ class Surface extends JPanel {
 	}
 }
 
-public class BasicEx {
+public class BasicEx extends JFrame {
+	public BasicEx() {
+		initUI();
+	}
+
+	private void initUI() {
+		add(new Surface());
+		
+		setTitle("Simple Java 2D example");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				BasicEx ex = new BasicEx();
+				ex.setVisible(true);
+			}
+		});
+	}
 }
